a06656b341d60f1f03e654bab67883ea448b5189
João Pedro Santos <52284888+jp4bidube@users.noreply.github.com>
2025-03-16 16:53:07 -0300

    chore: add kafka template and producer config

---
+++ patient-service/src/main/java/jp/projects/patientservice/kafka/KafkaProducer.java
@@ -0,0 +1,14 @@
+package jp.projects.patientservice.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+}
